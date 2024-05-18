13f917b5b82fcf08b9bc5788df6fba401cc19e73
Johnny8822 <146615330+Johnny8822@users.noreply.github.com>
2024-05-17 23:50:13 -0500

    Add files via upload

---
+++ include/env.h
@@ -0,0 +1,4 @@
+const char* ssid ="Digicel_WiFi_3FcH"; 
+const char* password = "";
+const char * output= "https://iot-project-xkbv.onrender.com/output";
+const char * update= "https://iot-project-xkbv.onrender.com/update";
